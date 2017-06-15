dc99e51cb6d2728c42acfb159093e0974b87b452
aldyadk <aldy.andika@gmail.com>
2017-06-15 16:17:12 +0700

    done all controller

---
+++ client/src/components/Main.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div v-for="article in articles">
+      {{article.title}}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Welcome to Hacktivpress',
+      articles: []
+    }
+  },
+  methods: {
+    getData: function () {
+      var self = this
+      axios.get(`http://localhost:3000/api/articles`)
+      .then((response) => {
+        self.articles = response.data
+        console.log(self.articles)
+      })
+    }
+  },
+  created: function () {
+    this.getData()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
